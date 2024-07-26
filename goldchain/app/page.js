8a9503a1fdd38169f54284bfa78a6e97c2b0e08a
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-07-26 09:15:23 +0530

    Added it to right

--- goldchain/app/page.js
+++ goldchain/app/page.js
@@ -1,18 +1,16 @@
 import Navbar from "./components/Navbar";
 import Animation from "./components/Animation/Animation";
+import Homescreen from "./components/HomeComponents/Home";
 
 export default function Home() {
   return (
-    <div>
+    <div className="font-myFont">
       <Navbar />
-      <div className="flex justify-end">
-        <Animation
-          url={
-            "https://lottie.host/634d3c3f-94d7-4370-a3d8-d3e5719a1f7d/9IuDJRRQFy.json"
-          }
-          width={400}
-          height={400}
-        />
+
+      <div className="">
+        <div className="relative left-56">
+          <Homescreen />
+        </div>
       </div>
     </div>
   );
