72e33745e4572a318fdd8d19f4d6e67620786a9b
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-11-06 00:42:30 +0530

    Added : register Gold section

--- goldchain/app/page.js
+++ goldchain/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
     console.table(data);
   };
   return (
-    <div className="font-myFon bg-black">
+    <div className="font-myFont">
       <Navbar />
 
       <div className="z-1">
@@ -21,14 +21,70 @@ export default function Home() {
           <Homescreen />
         </div>
       </div>
-      <div class="absolute top-100vh w-full left-56 flex justify-center -z-10">
-        <img className="opacity-30 w-full z-0" src="grid.svg" />
+      <div className="absolute w-full flex justify-center -z-10">
+        <img className="opacity-30 w-full " src="grid.svg" />
       </div>
-      <div className="ml-56 h-[100vh] bg-purple-500 z-10">
+      <div className="ml-56  z-10">
         <div className="">
-          <p>Welcome to 2nd section</p>
+          <p className="font-bold text-[2rem] flex justify-center">
+            Register Your Gold
+          </p>
         </div>
         <div className="">
+          <div className="flex">
+            <div className="flex flex-col mt-[5rem]">
+              <label className="mb-2  text-white" htmlFor="inputField">
+                Enter Name
+              </label>
+              <input
+                id="inputField"
+                type="text"
+                className="border border-gray-300 rounded-xl p-2 text-center w-64 text-black"
+                placeholder="Enter Buyer Name"
+              />
+              <label className="my-2  text-white" htmlFor="inputField">
+                Enter Aadhaar Number
+              </label>
+              <input
+                id="inputField"
+                type="number"
+                className="inline-block border border-gray-300 rounded-xl p-2 text-center w-64 text-black"
+                placeholder="Enter Aadhaar Number"
+              />
+            </div>
+            <div className="flex mt-10 ml-[30rem]">
+              <div>
+                <img src="" className="h-[10rem] w-[10rem]" />
+                <input
+                  id="fileUpload"
+                  className="my-2  rounded-xl p-2 w-64 cursor-pointer"
+                  type="file"
+                  accept="image/*"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="flex gap-x-10 my-5">
+            <label className="mb-2  text-white" htmlFor="inputField">
+              Enter Gold Weight
+            </label>
+            <input
+              id="inputField"
+              type="number"
+              className="border border-gray-300 rounded-xl p-2 text-center w-64 text-black"
+              placeholder="Enter Buyer Name"
+            />
+            <label className="my-2 justify-end text-white" htmlFor="inputField">
+              Enter Date
+            </label>
+            <input
+              id="inputField"
+              type="date"
+              className="border border-gray-300 rounded-xl p-2 text-center w-64 text-black"
+              placeholder="Enter Aadhaar Number"
+            />
+          </div>
           <button
             className="px-5 py-2 rounded-xl bg-yellow-400"
             onClick={() => getDetails()}
